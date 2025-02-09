465e3e82b9ba8e698a4e3645b5d53627976d2b5f
leonel_yomi <153129558+YOMILEONEL@users.noreply.github.com>
2025-02-09 22:42:54 +0100

    Spring Boot file added

---
+++ damespielServer/src/main/java/com/leonel/damespielServer/DamespielServerApplication.java
@@ -0,0 +1,13 @@
+package com.leonel.damespielServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamespielServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamespielServerApplication.class, args);
+	}
+
+}
---
+++ damespielServer/src/test/java/com/leonel/damespielServer/DamespielServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonel.damespielServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamespielServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
