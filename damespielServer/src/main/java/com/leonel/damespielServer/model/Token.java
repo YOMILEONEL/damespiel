6fbf0738ea517e1ae0e907fb8243bdb3bc72c4e6
leonel_yomi <153129558+YOMILEONEL@users.noreply.github.com>
2025-02-13 00:55:15 +0100

    Funktion to generate Random-GameId added

--- damespielServer/src/main/java/com/leonel/damespielServer/model/Token.java
+++ damespielServer/src/main/java/com/leonel/damespielServer/model/Token.java
@@ -1,24 +1,28 @@
 package com.leonel.damespielServer.model;
 
-
 import com.leonel.damespielServer.model.enumeration.Color;
 import com.leonel.damespielServer.model.enumeration.TokenType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Token {
 
     TokenType tokenType;
     boolean isKing;
     boolean isDelete;
 
+
+
     public String toString(){
         return tokenType.toString();
     }
+
     /**
      * Liefert die Farbe des Tokens basierend auf dem TokenType zurück.
      * @return die Farbe des Tokens (WHITE oder BLACK).
---
+++ damespielServer/src/main/java/com/leonel/damespielServer/utils/GameIdGenerator.java
@@ -0,0 +1,21 @@
+package com.leonel.damespielServer.utils;
+
+import java.security.SecureRandom;
+
+public class GameIdGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int ID_LENGTH = 7;
+    private static final SecureRandom random = new SecureRandom();
+
+    // Method to generate unique id of 7 characters
+    public static String generateGameId() {
+        StringBuilder gameId = new StringBuilder(ID_LENGTH);
+
+        for (int i = 0; i < ID_LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            gameId.append(CHARACTERS.charAt(randomIndex));
+        }
+
+        return gameId.toString();
+    }
+}
