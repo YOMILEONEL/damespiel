00494ac2174b31f8c74bd2fe194a045a92e87d2c
leonel_yomi <153129558+YOMILEONEL@users.noreply.github.com>
2025-02-14 21:24:50 +0100

    GameMapper added (get GameDto from Game)

---
+++ damespielServer/src/main/java/com/leonel/damespielServer/model/mapper/GameMapper.java
@@ -0,0 +1,24 @@
+package com.leonel.damespielServer.model.mapper;
+
+
+
+import com.leonel.damespielServer.model.Game;
+import com.leonel.damespielServer.model.dto.GameDTO;
+
+import java.util.stream.Collectors;
+
+public class GameMapper {
+
+    public static GameDTO toDTO(Game game) {
+        return GameDTO.builder()
+                .gameId(game.getGameId())
+                .gameStatus(game.getGameStatus())
+                .winner(game.getWinner())
+                .players(game.getPlayers().stream().map(PlayerMapper::toDTO).collect(Collectors.toList()))
+                .currentPlayerId(game.getCurrentPlayerId())
+                .playerColors(game.getPlayerColors())
+                .board(game.getBoard())
+                .build();
+    }
+
+}
