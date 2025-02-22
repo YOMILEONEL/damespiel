5e1151019614271aafdba8e3c3755d441dedb32c
leonel_yomi <153129558+YOMILEONEL@users.noreply.github.com>
2025-02-22 02:37:37 +0100

    loginPage and RegistrationPage added

---
+++ reactfrontend/src/app/login/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import NavBar from "../components/navBar";
+
+export default function Login() {
+  return (
+    <div className="page-container">
+      <NavBar />
+
+      <div className="main-container">
+        <div className="welcome-box">
+          <h1 className="title">Willkommen</h1>
+          <img src="https://bigthink.com/wp-content/uploads/2014/10/origin-55.jpg?resize=854" alt="Logo" width={50} height={50} />
+          <p className="subtitle">DAMESPIEL</p>
+        </div>
+
+        <h2 className="login-title">EINLOGEN</h2>
+        <form className="login-form">
+          
+        </form>
+      </div>
+      <input type="text" placeholder="Name" className="login-input" />
+          <input type="password" placeholder="Passwort" className="login-input" />
+          <button type="submit" className="login-button">einlogen</button>
+    </div>
+  );
+}
